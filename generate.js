3619fd78a45817f2e2d1081974c5654f8eedcb12
Fabrizio Armango <fabrizio.armango@gmail.com>
2022-10-13 13:10:26 +0200

    added arguments

--- generate.js
+++ generate.js
@@ -1,10 +1,28 @@
 #!/usr/bin/env node
 "use strict";
-
-const { cwd } = require("process");
+const yargs = require("yargs");
+const { cwd, exit } = require("process");
+const path = require("node:path")
 const { readFileSync, writeFileSync } = require("fs");
 
-const routesManifest = "./.next/routes-manifest.json";
+const argv = yargs
+  .option("routes-manifest-file", {
+    alias: "f",
+    description: "Routes manifest",
+    type: "string",
+    default: "./.next/routes-manifest.json",
+  })
+  .option("output-file", {
+    alias: "o",
+    description: "Output file path",
+    type: "string",
+    default: "./next-routes.conf"
+  })
+  .help()
+  .alias("help", "h").argv;
+
+const routesManifest = argv["routes-manifest-file"];
+const outputFile = argv["output-file"];
 const manifest = JSON.parse(readFileSync(routesManifest, "utf8"));
 
 const routes = manifest.staticRoutes
@@ -19,6 +37,6 @@ location ~ ${route.regex} {
 }`;
   });
 
-writeFileSync("./next-routes.conf", routes.join("\n"));
+writeFileSync(outputFile, routes.join("\n"));
 
-console.log(`Nginx routes configuration written to ${cwd()}/next-routes.conf`);
+console.log(`Nginx routes configuration written to ${path.join(cwd(), outputFile)}`);
